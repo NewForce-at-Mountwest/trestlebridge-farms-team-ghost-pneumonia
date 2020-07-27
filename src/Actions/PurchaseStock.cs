05a4b439bfcf08a1183c1780057d0dee29624480
Dylan Bishop <mdylbishop@gmail.com>
2020-07-27 14:36:45 -0400

    created interfaces and animal types

--- src/Actions/PurchaseStock.cs
+++ src/Actions/PurchaseStock.cs
@@ -8,7 +8,12 @@ namespace Trestlebridge.Actions {
     public class PurchaseStock {
         public static void CollectInput (Farm farm) {
             Console.WriteLine ("1. Cow");
-            Console.WriteLine ("2. Ostrich");
+            Console.WriteLine ("2. Pig");
+            Console.WriteLine ("3. Chicken");
+            Console.WriteLine ("4. Duck");
+            Console.WriteLine ("5. Goat");
+            Console.WriteLine ("6. Ostrich");
+            Console.WriteLine ("7. Sheep");
 
             Console.WriteLine ();
             Console.WriteLine ("What are you buying today?");
